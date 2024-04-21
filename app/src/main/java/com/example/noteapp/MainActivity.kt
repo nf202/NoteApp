3eb02c21e757a9e0e948338de6483dff4efbe910
nf202 <2715729066@qq.com>
2024-04-21 19:00:52 +0800

    基础页面设计

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.noteapp
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    // 在这里添加你的代码逻辑
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 在这里添加你的代码逻辑
     }
 }
